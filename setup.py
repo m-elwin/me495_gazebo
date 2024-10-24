af0738988ea455b9f8417117c70cf42e9859cd95
Matthew Elwin <elwin@northwestern.edu>
2024-10-23 22:43:17 -0500

    Made setup.py recurse subdirectories for models and worlds.
    All the files in the subdirectories are needed.

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import find_packages, setup
-from glob import glob
+from pathlib import Path
+
+def recursive_files(prefix, path):
+    """
+    Recurse over path returning a list of tuples suitable for use with setuptools data_files.
+    :param prefix: prefix path to prepend to the path
+    :param path: Path to directory to recurse. Path should not have a trailing '/'
+    :return: List of tuples. First element of each tuple is destination path, second element is a list of files to copy to that path
+    """
+    return [(str(Path(prefix)/subdir),
+             [str(file) for file in subdir.glob('*') if not file.is_dir()] ) for subdir in Path(path).glob('**')]
 
 package_name = 'me495_gazebo'
 
@@ -12,8 +22,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/env-hooks', ['env-hooks/me495_gazebo.dsv']),
-        ('share/' + package_name + '/models', glob("models/*.sdf")),
-        ('share/' + package_name + '/worlds', glob("worlds/*.sdf"))
+        *recursive_files('share/' + package_name, "models"),
+        *recursive_files('share/' + package_name, "worlds")
     ],
     install_requires=['setuptools'],
     zip_safe=True,
